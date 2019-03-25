f988b2b6d07e8e1941767cb2e21250bd8b54204a
Denis <denis.da.by@gmail.com>
2019-03-25 11:54:43 +0300

    1

--- src/XmlParser.php
+++ src/XmlParser.php
@@ -10,15 +10,27 @@ class XmlParser extends BaseXmlParserViaXmlReader
 	 * @param \XMLReader $xml
 	 * @return mixed|null|string
 	 */
-	protected function xmlReaderToAssoc(\XMLReader $xml) {
+	protected function xmlReaderToAssoc(\XMLReader $xml)
+	{
 		$tree = null;
 		while ($xml->read()) {
+
+			//если нода закончилась врзавращает что насобирали
 			if ($xml->nodeType == \XMLReader::END_ELEMENT) {
 				return $tree;
-			} else if ($xml->nodeType == \XMLReader::ELEMENT) {
+				//если в ноде есть вложения то
+			}
+			else if ($xml->nodeType == \XMLReader::ELEMENT) {
+
 				if ((!$xml->isEmptyElement)) {
+//					$name = $xml->localName;
+					//получим деток
 					$children = $this->xmlReaderToAssoc($xml);
+
+
 					if (is_array($children) and count($children) > 1) {
+
+
 						if (isset($tree[$xml->localName])) {
 							if (!isset($tree[$xml->localName][0])) {
 								$moved = $tree[$xml->localName];
@@ -26,14 +38,32 @@ class XmlParser extends BaseXmlParserViaXmlReader
 								$tree[$xml->localName][0] = $moved;
 							}
 							$tree[$xml->localName][] = $children;
-						} else {
+						}
+						else {
 							$tree[$xml->localName] = $children;
 						}
-					} else {
-						$tree[$xml->localName] = $children;
+
+
 					}
+					else {
+						if (!isset($tree[$xml->localName])) {
+							$tree[$xml->localName] = $children;
+						}
+						else if (is_array($tree[$xml->localName])){
+							$tree[$xml->localName][] = $children;
+						} else{
+							$tree[$xml->localName] = [$tree[$xml->localName],$children];
+						}
+
+					}
+
+
+					//если в ноде есть вложения то
 				}
-			} else if ($xml->nodeType == \XMLReader::TEXT) {
+
+				//если в ноде текст возвращает его
+			}
+			else if ($xml->nodeType == \XMLReader::TEXT) {
 				$tree = $xml->value;
 			}
 		}
